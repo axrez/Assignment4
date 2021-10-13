62eef62a91ed46bb7875d5a8c66a2a9c629dead2
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-10-13 21:07:50 +0200

    Implement read operations

--- Assignment4.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment4.Entities.Tests/TaskRepositoryTests.cs
@@ -1,10 +1,341 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Assignment4.Core;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Assignment4.Entities.Tests
 {
-    public class TaskRepositoryTests
-    {
+	/*
+	There is an issue inserting tags into the Sqlite database. I couldn't fix it, so i just ignore tags for now.
+	*/
+	public class TaskRepositoryTests : IDisposable
+	{
+		#region Setup
+		private readonly KanbanContext _context;
+		private readonly TaskRepository _repository;
 
-    }
+		#region Tags
+		private readonly Tag assignment = new Tag { Name = "Assignment" };
+		private readonly Tag test = new Tag { Name = "Test" };
+		private readonly Tag extra = new Tag { Name = "Extra" };
+		private readonly Tag confusing = new Tag { Name = "Confusing" };
+		#endregion
+
+		public TaskRepositoryTests()
+		{
+			// Create Sqlite database and connect to it
+			var connection = new SqliteConnection("Filename=:memory:");
+			connection.Open();
+			var builder = new DbContextOptionsBuilder<KanbanContext>();
+			builder.UseSqlite(connection);
+			builder.EnableSensitiveDataLogging(true);
+			var context = new KanbanContext(builder.Options);
+			context.Database.EnsureCreated();
+
+			// Create test data
+			#region Tasks
+			var implementTaskRepo = new Task {
+					Id = 1,
+					Title = "Implement TaskRepository",
+					Description = "Implement ITaskRepository in the TaskRepository class.",
+					state = State.Active,
+					tags = new[] { assignment, test }
+				};
+			var cryOverErrors = new Task
+			{
+				Id = 2,
+				Title = "Cry over errors",
+				Description = "WHY DOES IT NOT JUST WORK!?!?!?",
+				state = State.Resolved,
+				tags = new[] { confusing }
+			};
+			var addTheThing = new Task
+			{
+				Id = 3,
+				Title = "Add the thing",
+				Description = "Sorry nvm.",
+				state = State.New,
+				tags = new[] { extra, confusing }
+			};
+			var setupStuff = new Task
+			{
+				Id = 4,
+				Title = "Setup stuff",
+				Description = "The stuff needs to be set up.",
+				state = State.Closed,
+				tags = new[] { confusing, test }
+			};
+			var badIdea = new Task
+			{
+				Id = 5,
+				Title = "Bad idea",
+				Description = "Trust me, it'll be great!",
+				state = State.Removed,
+				tags = new Tag[] { }
+			};
+			#endregion
+
+			#region Users
+			var magnus = new User
+			{
+				Id = 1,
+				Email = "daml@itu.dk",
+				Name = "Magnus",
+				tasks = new[] { implementTaskRepo, cryOverErrors }
+			};
+			var emil = new User
+			{
+				Id = 2,
+				Email = "emio@itu.dk",
+				Name = "Emil",
+				tasks = new[] { addTheThing, setupStuff }
+			};
+			#endregion
+
+			context.Users.AddRange(
+				magnus,
+				emil
+			);
+			context.Tasks.Add(badIdea);
+
+			context.SaveChanges();
+
+			_context = context;
+			_repository = new TaskRepository(_context);
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+		}
+		#endregion
+
+		#region Read
+		[Fact]
+		public void Read_Nonexisting_Returns_Null()
+		{
+			// Arrange
+			var id = 6;
+			// Act
+			var task = _repository.Read(id);
+			// Assert
+			Assert.Null(task);
+		}
+
+		[Fact]
+		public void Read_Existing_Returns_Task_With_Id()
+		{
+			// Arrange
+			var id = 1;
+			// Act
+			var task = _repository.Read(id);
+			// Assert
+			Assert.Equal(id, task.Id);
+			Assert.Equal("Implement TaskRepository", task.Title);
+			Assert.Equal("Implement ITaskRepository in the TaskRepository class.", task.Description);
+			Assert.Equal(new DateTime(), task.Created);
+			Assert.Equal(null, task.AssignedToName);
+			Assert.Equal(State.Active, task.State);
+			Assert.Equal(new DateTime(), task.StateUpdated);
+		}
+
+		[Fact]
+		public void Read_Add_Returns_All()
+		{
+			// Arrange
+			var expected = new[] {
+				new TaskDTO(
+					1,
+					"Implement TaskRepository",
+					null,
+					new[] { assignment.Name, test.Name },
+					State.Active
+				),
+				new TaskDTO(
+					2,
+					"Cry over errors",
+					null,
+					new[] { confusing.Name },
+					State.Resolved
+				),
+				new TaskDTO(
+					3,
+					"Add the thing",
+					null,
+					new[] { extra.Name, confusing.Name },
+					State.New
+				),
+				new TaskDTO(
+					4,
+					"Setup stuff",
+					null,
+					new[] { confusing.Name, test.Name },
+					State.Closed
+				),
+				new TaskDTO(
+					5,
+					"Bad idea",
+					null,
+					new string[] { },
+					State.Removed
+				)
+			};
+			// Act
+			var tasks = _repository.ReadAll();
+			var comparer = new TaskDTOComparer();
+			// Assert
+			Assert.Equal(expected, tasks, comparer);
+		}
+
+		[Fact]
+		public void ReadAllByState_Returns_All_With_State()
+		{
+			// Arrange
+			var expected = new[] {
+				new TaskDTO(
+					1,
+					"Implement TaskRepository",
+					null,
+					new[] { assignment.Name, test.Name },
+					State.Active
+				)
+			};
+			// Act
+			var tasks = _repository.ReadAllByState(State.Active);
+			var comparer = new TaskDTOComparer();
+			// Assert
+			Assert.Equal(expected, tasks, comparer);
+		}
+
+		[Fact]
+		public void ReadAllRemoved_Returns_All_Removed()
+		{
+			// Arrange
+			var expected = new[] {
+				new TaskDTO(
+					5,
+					"Bad idea",
+					null,
+					new string[] { },
+					State.Removed
+				)
+			};
+			// Act
+			var tasks = _repository.ReadAllRemoved();
+			var comparer = new TaskDTOComparer();
+			// Assert
+			Assert.Equal(expected, tasks, comparer);
+		}
+
+		[Fact]
+		public void ReadAllByTag_Returns_All_With_Tag()
+		{
+			// Arrange
+			var expected = new[] {
+				new TaskDTO(
+					2,
+					"Cry over errors",
+					null,
+					new[] { confusing.Name },
+					State.Resolved
+				),
+				new TaskDTO(
+					3,
+					"Add the thing",
+					null,
+					new[] { extra.Name, confusing.Name },
+					State.New
+				),
+				new TaskDTO(
+					4,
+					"Setup stuff",
+					null,
+					new[] { confusing.Name, test.Name },
+					State.Closed
+				)
+			};
+			// Act
+			var tasks = _repository.ReadAllByTag("Confusing");
+			var comparer = new TaskDTOComparer();
+			// Assert
+			Assert.Equal(expected, tasks, comparer);
+		}
+
+		[Fact]
+		public void ReadAllByUser_Returns_All_With_User()
+		{
+			// Arrange
+			var expected = new[] {
+				new TaskDTO(
+					3,
+					"Add the thing",
+					null,
+					new[] { extra.Name, confusing.Name },
+					State.New
+				),
+				new TaskDTO(
+					4,
+					"Setup stuff",
+					null,
+					new[] { confusing.Name, test.Name },
+					State.Closed
+				)
+			};
+			// Act
+			var tasks = _repository.ReadAllByUser(2);
+			var comparer = new TaskDTOComparer();
+			// Assert
+			Assert.Equal(expected, tasks, comparer);
+		}
+		#endregion
+
+		private class TaskDetailsDTOComparer : IEqualityComparer<TaskDetailsDTO>
+		{
+			public bool Equals(TaskDetailsDTO x, TaskDetailsDTO y)
+			{
+				var isEqual = x.Id == y.Id
+					       && x.Title == y.Title
+					       && x.Description == y.Description
+					       && x.Created == y.Created
+					       && x.AssignedToName == y.AssignedToName
+					       && x.State == y.State
+					       && x.StateUpdated == y.StateUpdated;
+				if (isEqual == false) return false;
+				if (x.Tags == null)
+					return y.Tags == null;
+				else
+					return x.Tags.OrderBy(s => s).SequenceEqual(y.Tags.OrderBy(s => s));
+			}
+
+			public int GetHashCode([DisallowNull] TaskDetailsDTO obj)
+			{
+				return obj.GetHashCode();
+			}
+		}
+
+		private class TaskDTOComparer : IEqualityComparer<TaskDTO>
+		{
+			public bool Equals(TaskDTO x, TaskDTO y)
+			{
+				var isEqual = x.Id == y.Id
+					       && x.Title == y.Title
+					       && x.AssignedToName == y.AssignedToName
+					       && x.State == y.State;
+				if (isEqual == false) return false;
+				if (x.Tags == null)
+					return y.Tags == null;
+				else
+					return x.Tags.OrderBy(s => s).SequenceEqual(y.Tags.OrderBy(s => s));
+			}
+
+			public int GetHashCode([DisallowNull] TaskDTO obj)
+			{
+				return obj.GetHashCode();
+			}
+		}
+	}
 }
--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -1,6 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assignment4.Core;
+using static Assignment4.Core.Response;
+
 namespace Assignment4.Entities
 {
-    public class TaskRepository
-    {
-    }
+	public class TaskRepository : ITaskRepository
+	{
+        private readonly KanbanContext _context;
+
+		public TaskRepository(KanbanContext context)
+		{
+			_context = context;
+		}
+
+		#region Create
+		public (Response Response, int TaskId) Create(TaskCreateDTO task)
+		{
+			throw new System.NotImplementedException();
+		}
+        #endregion
+
+        #region Delete
+		public Response Delete(int taskId)
+		{
+			throw new System.NotImplementedException();
+		}
+        #endregion
+
+        #region Read
+		public TaskDetailsDTO Read(int taskId)
+		{
+			var tasks =
+                from t in _context.Tasks
+                where t.Id == taskId
+                select new TaskDetailsDTO(
+                    t.Id,
+                    t.Title,
+                    t.Description,
+                    t.Created,
+                    t.AssignedToName,
+                    t.tags.Select(x => x.Name).ToArray(),
+                    t.state,
+                    t.StateUpdated
+                );
+            
+            return tasks.FirstOrDefault();
+		}
+
+		public IReadOnlyCollection<TaskDTO> ReadAll()
+		{
+			var tasks =
+                from t in _context.Tasks
+                select new TaskDTO(
+                    t.Id,
+                    t.Title,
+                    t.AssignedToName,
+                    t.tags.Select(x => x.Name).ToArray(),
+                    t.state
+                );
+            
+            return tasks.ToArray();
+		}
+
+		public IReadOnlyCollection<TaskDTO> ReadAllByState(State state)
+		{
+			var tasks =
+                from t in _context.Tasks
+                where t.state == state
+                select new TaskDTO(
+                    t.Id,
+                    t.Title,
+                    t.AssignedToName,
+                    t.tags.Select(x => x.Name).ToArray(),
+                    t.state
+                );
+            
+            return tasks.ToArray();
+		}
+
+		public IReadOnlyCollection<TaskDTO> ReadAllByTag(string tag)
+		{
+			var tasks =
+                from t in _context.Tasks
+                where t.tags.Select(x => x.Name).Contains(tag)
+                select new TaskDTO(
+                    t.Id,
+                    t.Title,
+                    t.AssignedToName,
+                    t.tags.Select(x => x.Name).ToArray(),
+                    t.state
+                );
+
+            return tasks.ToArray();
+		}
+
+		public IReadOnlyCollection<TaskDTO> ReadAllByUser(int userId)
+		{
+            var user = (
+                from u in _context.Users
+                where u.Id == userId
+                select u
+            ).FirstOrDefault();
+
+            var tasks =
+                from t in user.tasks
+                select new TaskDTO(
+                    t.Id,
+                    t.Title,
+                    t.AssignedToName,
+                    t.tags.Select(x => x.Name).ToArray(),
+                    t.state
+                );
+
+            return tasks.ToArray();
+		}
+
+		public IReadOnlyCollection<TaskDTO> ReadAllRemoved()
+		{
+			return ReadAllByState(State.Removed);
+		}
+        #endregion
+        
+        #region  Update
+		public Response Update(TaskUpdateDTO task)
+		{
+			throw new System.NotImplementedException();
+		}
+        #endregion
+	}
 }
